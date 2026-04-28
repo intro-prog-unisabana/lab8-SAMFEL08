55ad3169204fea6be252a27f082bfa6047a47bc9
Samuel G <gonzalezsam451@gmail.com>
2026-04-27 21:38:17 -0500

    prueba

--- todo_manager.py
+++ todo_manager.py
@@ -3,11 +3,25 @@
 
 def read_todo_file(file_path):
     """Reads tasks from a file. Returns a list of tasks."""
-    # TODO: Implementar manejo de FileNotFoundError según README.md
-    raise NotImplementedError
+    try:
+        archivo = open(file_path, "r")
+        lista = []
+
+        for linea in archivo:
+            lista.append(linea.strip())
+
+        archivo.close()
+        return lista
+
+    except:
+        print(f"File {file_path} not found! Returning an empty to-do list.")
+        return []
 
 
 def write_todo_file(file_path, tasks):
-    """Writes tasks to a file, one per line."""
-    # TODO: Implementar escritura de tareas según README.md
-    raise NotImplementedError
+    archivo = open(file_path, "w")
+
+    for tarea in tasks:
+        archivo.write(tarea + "\n")
+
+    archivo.close()
